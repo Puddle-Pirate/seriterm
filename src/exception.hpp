300af2fb5141b7f4f12af65fba319223782cc08e
Ryan <100020750+Puddle-Pirate@users.noreply.github.com>
2025-07-20 12:10:37 +1200

    Add custom Assert'ing

--- src/exception.hpp
+++ src/exception.hpp
@@ -16,3 +16,17 @@ public:
    explicit TerminalException(const std::string& message)
       : std::runtime_error("Terminal error: " + message) {}
 };
+
+class Assert
+{
+public:
+   static void That(bool condition)
+   {
+      if (!condition) throw std::logic_error("Assert failed");
+   }
+
+   static void That(bool condition, std::string const& message)
+   {
+      if (!condition) throw std::logic_error("Assert failed: " + message);
+   }
+};
